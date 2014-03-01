8a04402d0b6ac40b40a2ff53d38c047eef7f742e
Monica Thornton <monicasuecooper@hotmail.com>
2014-02-28 23:14:28 -0700

    Lab 2, Python
    Python lab to play Rock, Paper, Scissors, Lizard, Spock implemented
    using OO paradigms.

---
+++ Thornton_Monica.lab2.py
@@ -0,0 +1,15 @@
+######################################### 	
+#  CSCI 305 - Programming Lab #2		
+#										
+#  Monica Thornton			
+#  monicasuethornton@gmail.com			
+#										
+#########################################
+
+# Replace the string value of the following variable with your names.
+myName = "Monica Thornton"
+
+# I did not work with a partner on this lab
+# my $partner = "<Replace with your partner's name>";
+
+print ("CSCI 305 Lab 2 submitted by " + myName + ".")
