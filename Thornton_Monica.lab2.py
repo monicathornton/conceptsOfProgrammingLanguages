a8d86254cd46e952ec68525ef9a7d51811b2511a
Monica Thornton <monicasuecooper@hotmail.com>
2014-03-20 01:19:33 -0600

    Finished commenting
    Implemented the bones of the player class

--- Thornton_Monica.lab2.py
+++ Thornton_Monica.lab2.py
@@ -25,229 +25,208 @@ class Element (object):
     def __init__(self, _name):
          self._name = _name
 
-    #getter method to return the instance variable _name for the particular element (inherited in subclasses)
+    #getter method to return the instance variable _name for the particular Element (inherited in subclasses)
     def name(self):
          return self._name
 
-    """An abstract method to compare Elements, to determine the winner when pitted against eachother. Each subclass
-    will override this method, and we will not be comparing Element objects to eachother directly, so we throw an exception
-    if someone tries to compare two Element objects directly. """
+    """An abstract method to compare Elements, to determine the win status/method of winning when pitted against eachother.
+    Each subclass will override this method in different ways, and we will not be comparing Element objects to eachother directly,
+    so we throw an exception if someone tries to compare two Element objects directly. """
     def compareTo(self, element):
          raise NotImplementedError("Not yet implemented")
 
 """A subclass to represent the Rock element, which triumphs when pitted against Lizard or Rock,
 but perishes against Paper or Spock. If Rock is played against itself in a match, it results in a tied game."""
 class Rock (Element):
-    #the name instance variable from Element is overriden, and its value set to Rock
+    #The name instance variable from Element is overriden, and its value set to Rock
     _name = "Rock"
 
     #Constructor to build Rocks
     def __init__(self,name):
         super(Rock,self).__init__("Rock")
-         
-    def compareTo(self, element):
-        #A winning case, Rock crushes Lizard
-        if (element.name() == "Lizard"):
-            return ("Rock crushes Lizard", "Win")
 
-        #A winning case, Rock crushes Scissors
-        if (element.name() == "Scissors"):
-            return ("Rock crushes Scissors", "Win")
+    #Overrides the compareTo method inherited from element, with all the particular win/loss info for this Element     
+    def compareTo(self, Element):
+        #A winning case, represents the case when Rock (the self parameter) is played against Lizard(the Element parameter)
+        if (Element.name() == "Lizard"):
+            return (self.name() + " crushes " + Element.name(), "Win")
 
-        #Tie case, where both players throw Rock (Rock equals Rock)
-        if (self == element):
-            return ("Rock equals Rock", "Tie")  
+        #A winning case, represents the case when Rock (the self parameter) is played against Scissors(the Element parameter)
+        if (Element.name() == "Scissors"):
+            return (self.name() + " crushes " + Element.name(), "Win")
 
-        #A losing case, Paper covers Rock
-        if (element.name() == "Paper"):
-            return ("Paper covers Rock", "Lose")        
+        #Tie case, where both players throw Rock
+        if (self.name() == Element.name()):
+            return (self.name() + " equals " + Element.name(), "Tie")  
 
-        #A losing case, Spock vaporizes Rock
-        if (element.name() == "Spock"):
-            return ("Spock vaporizes Rock", "Lose")         
+        #A losing case, represents the case when Rock (the self parameter) is played against Paper (the Element parameter)
+        if (Element.name() == "Paper"):
+            return (Element.name() + " covers " + self.name(), "Lose")        
 
+        #A losing case, represents the case when Rock (the self parameter) is played against Spock (the Element parameter)
+        if (Element.name() == "Spock"):
+            return (Element.name() + " vaporizes " + self.name(), "Lose")         
+
+"""A subclass to represent the Paper element, which triumphs when pitted against Rock or Spock,
+but perishes against Scissors or Lizard. If Paper is played against itself in a match, it results in a tied game."""
 class Paper (Element):
-    #the name instance variable from Element is overriden, and its value set to Rock
+    #the name instance variable from Element is overriden, and its value set to Paper
     _name = "Paper"
 
-    #Constructor to build Rocks
+    #Constructor to build Papers
     def __init__(self,name):
         super(Paper,self).__init__("Paper")
-         
-    def compareTo(self, element):
-        #A winning case, Paper covers Rock
-        if (element.name() == "Rock"):
-            return ("Paper covers Rock", "Win")
 
-        #A winning case, Paper disproves Spock
-        if (element.name() == "Spock"):
-            return ("Paper disproves Spock", "Win")
+    #Overrides the compareTo method inherited from element, with all the particular win/loss info for this element              
+    def compareTo(self, Element):
+        #A winning case, represents the case when Paper (the self parameter) is played against Rock (the Element parameter)
+        if (Element.name() == "Rock"):
+            return (self.name() + " covers " + Element.name(), "Win")
 
-        #Tie case, where both players throw Paper (Paper equals Paper)
-        if (self == element):
-            return ("Paper equals Paper", "Tie")  
+        #A winning case, represents the case when Paper (the self parameter) is played against Spock (the Element parameter)
+        if (Element.name() == "Spock"):
+            return (self.name() + " disproves " + Element.name(), "Win")
 
-        #A losing case, Scissors cut Paper
-        if (element.name() == "Scissors"):
-            return ("Scissors cut Paper", "Lose")        
+        #Tie case, where both players throw Paper
+        if (self.name() == Element.name()):
+            return (self.name() + " equals " + Element.name(), "Tie")  
 
-        #A losing case, Lizard eats Paper
-        if (element.name() == "Lizard"):
-            return ("Lizard eats Paper", "Lose")      
+        #A losing case, represents the case when Paper (the self parameter) is played against Scissors (the Element parameter)
+        if (Element.name() == "Scissors"):
+            return (Element.name() + " cut " + self.name(), "Lose")        
 
+        #A losing case, represents the case when Paper (the self parameter) is played against Lizard (the Element parameter)
+        if (Element.name() == "Lizard"):
+            return (Element.name() + " eats " + self.name(), "Lose")      
+
+"""A subclass to represent the Scissors element, which triumphs when pitted against Paper or Lizard,
+but perishes against Spock or Rock. If Scissors is played against itself in a match, it results in a tied game."""
 class Scissors (Element):
-    #the name instance variable from Element is overriden, and its value set to Rock
+    #the name instance variable from Element is overriden, and its value set to Scissors
     _name = "Scissors"
 
     #Constructor to build Scissors
     def __init__(self,name):
         super(Scissors,self).__init__("Scissors")
-         
-    def compareTo(self, element):
-        #A winning case, Scissors cut Paper
-        if (element.name() == "Paper"):
-            return ("Scissors cut Paper", "Win")
+
+    #Overrides the compareTo method inherited from element, with all the particular win/loss info for this element            
+    def compareTo(self, Element):
+        #A winning case, represents the case when Scissors (the self parameter) is played against Paper (the Element parameter)
+        if (Element.name() == "Paper"):
+            return (self.name() + " cut " + Element.name(), "Win")
 
         #A winning case, Scissors decapitate Lizard
-        if (element.name() == "Lizard"):
-            return ("Scissors decapitate Lizard", "Win")
+        if (Element.name() == "Lizard"):
+            return (self.name() + " decapitate " + Element.name(), "Win")
 
         #Tie case, where both players throw Scissors (Scissors equals Scissors)
-        if (self == element):
-            return ("Scissors equals Scissors", "Tie")  
+        if (self.name() == Element.name()):
+            return (self.name() + " equals " + Element.name(), "Tie")  
 
         #A losing case, Spock smashes Scissors
-        if (element.name() == "Spock"):
-            return ("Spock smashes Scissors", "Lose")        
+        if (Element.name() == "Spock"):
+            return (Element.name() + " smashes " + self.name(), "Lose")        
 
         #A losing case, Rock crushes Scissors
-        if (element.name() == "Rock"):
-            return ("Rock crushes Scissors", "Lose")    
+        if (Element.name() == "Rock"):
+            return (Element.name() + " crushes " + self.name(), "Lose")    
 
+"""A subclass to represent the Lizard element, which triumphs when pitted against Spock or Paper,
+but perishes against Rock or Scissors. If Lizard is played against itself in a match, it results in a tied game."""
 class Lizard (Element):
-    #the name instance variable from Element is overriden, and its value set to Rock
+    #the name instance variable from Element is overriden, and its value set to Lizard
     _name = "Lizard"
 
-    #Constructor to build Scissors
+    #Constructor to build Lizard
     def __init__(self,name):
         super(Lizard,self).__init__("Lizard")
-         
-    def compareTo(self, element):
-        #A winning case, Lizard poisons Spock
-        if (element.name() == "Spock"):
-            return ("Lizard poisons Spock", "Win")
 
-        #A winning case, Lizard eats Paper
-        if (element.name() == "Paper"):
-            return ("Lizard eats Paper", "Win")
+    #Overrides the compareTo method inherited from element, with all the particular win/loss info for this element           
+    def compareTo(self, Element):
+        #A winning case, represents the case when Lizard (the self parameter) is played against Spock (the Element parameter)
+        if (Element.name() == "Spock"):
+            return (self.name() + " poisons " + Element.name(), "Win")
 
-        #Tie case, where both players throw Lizard (Lizard equals Lizard)
-        if (self == element):
-            return ("Lizard equals Lizard", "Tie")  
+        #A winning case, represents the case when Lizard (the self parameter) is played against Paper (the Element parameter)
+        if (Element.name() == "Paper"):
+            return (self.name() + " eats " + Element.name(), "Win")
 
-        #A losing case, Rock crushes Lizard
-        if (element.name() == "Rock"):
-            return ("Rock crushes Lizard", "Lose")        
+        #Tie case, where both players throw Lizard
+        if (self.name() == Element.name()):
+            return (self.name() + " equals " + Element.name(), "Tie")  
 
-        #A losing case, Scissors decapitate Lizard
-        if (element.name() == "Scissors"):
-            return ("Scissors decapitate Lizard", "Lose")    
+        #A losing case, represents the case when Lizard (the self parameter) is played against Rock (the Element parameter) 
+        if (Element.name() == "Rock"):
+            return (Element.name() + " crushes " + self.name(), "Lose")        
 
+        #A losing case, represents the case when Lizard (the self parameter) is played against Lizard (the Element parameter) 
+        if (Element.name() == "Scissors"):
+            return (Element.name() + " decapitate " + self.name(), "Lose")    
+
+"""A subclass to represent the Spock element, which triumphs when pitted against Scissors or Rock,
+but perishes against Paper or Lizard. If Spock is played against itself in a match, it results in a tied game."""
 class Spock (Element):
     #the name instance variable from Element is overriden, and its value set to Rock
     _name = "Spock"
 
-    #Constructor to build Scissors
+    #Constructor to build Spocks
     def __init__(self,name):
         super(Spock,self).__init__("Spock")
-         
-    def compareTo(self, element):
-        #A winning case, Spock smashes Scissors
-        if (element.name() == "Scissors"):
-            return ("Spock smashes Scissors", "Win")
+        
+    #Overrides the compareTo method inherited from element, with all the particular win/loss info for this element            
+    def compareTo(self, Element):
+        #A winning case, represents the case when Spock (the self parameter) is played against Scissors (the Element parameter)
+        if (Element.name() == "Scissors"):
+            return (self.name() + " smashes " + Element.name(), "Win")
 
-        #A winning case, Spock vaporizes Rock
-        if (element.name() == "Rock"):
-            return ("Spock vaporizes Rock", "Win")
+        #A winning case, represents the case when Spock (the self parameter) is played against Rock (the Element parameter)
+        if (Element.name() == "Rock"):
+            return (self.name() + " vaporizes " + Element.name(), "Win")
 
-        #Tie case, where both players throw Spock (Spock equals Spock)
-        if (self == element):
-            return ("Spock equals Spock", "Tie")  
+        #Tie case, where both players throw Spock 
+        if (self.name() == Element.name()):
+            return (self.name() + " equals " + Element.name() + " - MIND MELD!!!", "Tie")  
 
-        #A losing case, Paper disproves Spock
-        if (element.name() == "Paper"):
-            return ("Paper disproves Spock", "Lose")        
+        #A losing case, represents the case when Spock (the self parameter) is played against Paper (the Element parameter) 
+        if (Element.name() == "Paper"):
+            return (Element.name() + " disproves " + self.name(), "Lose")        
 
-        #A losing case, Lizard poisons Spock
-        if (element.name() == "Lizard"):
-            return ("Lizard poisons Spock", "Lose")    
+        #A losing case, represents the case when Spock (the self parameter) is played against Lizard (the Element parameter)  
+        if (Element.name() == "Lizard"):
+            return (Element.name() + " poisons " + self.name(), "Lose")    
 
-#rock = Rock()
-#paper = Paper(`Paper')
-#print rock.compareTo(paper)
-#print paper.compareTo(rock)
-#print rock.compareTo(rock)
-#result1 = Element('Rock') 
-#result1.compareTo('Rock')
-
-rock = Rock("Rock")
-paper = Paper("Paper")
-scissors = Scissors("Scissors")
-lizard = Lizard("Lizard")
-spock = Spock("Spock")
-
-print("Testing Rock")
-print(rock.compareTo(lizard))
-print(rock.compareTo(scissors))
-print(rock.compareTo(rock))
-print(rock.compareTo(paper))
-print(rock.compareTo(spock))
-print("_____________")
-print("Testing Paper")
-print(paper.compareTo(rock))
-print(paper.compareTo(spock))
-print(paper.compareTo(paper))
-print(paper.compareTo(scissors))
-print(paper.compareTo(lizard))
-print("_____________")
-print("Testing Scissors")
-print(scissors.compareTo(paper))
-print(scissors.compareTo(lizard))
-print(scissors.compareTo(scissors))
-print(scissors.compareTo(spock))
-print(scissors.compareTo(rock))
-print("_____________")
-print("Testing Lizard")
-print(lizard.compareTo(spock))
-print(lizard.compareTo(paper))
-print(lizard.compareTo(lizard))
-print(lizard.compareTo(rock))
-print(lizard.compareTo(scissors))
-print("_____________")
-print("Testing Spock")
-print(spock.compareTo(scissors))
-print(spock.compareTo(rock))
-print(spock.compareTo(spock))
-print(spock.compareTo(paper))
-print(spock.compareTo(lizard))
-print("_____________")
-
-def init_list():
-    # global variable, can be used anywhere within the file after
-    # init_list gets called, since it's declared with "global" keyword
+"""A method to create a concrete instance of each of the five Element subclasses, and to store each of these
+in a global list named moves"""
+def moves_list():
+    #Creates a concrete instance of Rock, Paper, Scissors, Lizard, Spock, saves them with appropriate variable names
     rock = Rock("Rock")
     paper = Paper("Paper")
     scissors = Scissors("Scissors")
     lizard = Lizard("Lizard")
     spock = Spock("Spock")
-    
+
+    #Creates a global variable named moves
     global moves
+
+    #Constructs a list of all possible moves a player could throw, saves it in the (global) variable moves 
     moves = [rock, paper, scissors, lizard, spock]
 
-    for i in range(len(moves)):
-        print(moves[i])
+#Calls the moves list function, so the moves are instantiated and stored in a list
+moves_list()
 
+"""COMMENt HERE DOWN!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!"""
+class Player (object):
+    #an instance variable to store the name of each Player
+    _name = ""
 
-init_list()
-print("Does this work?")
-print(moves[0].compareTo(moves[1]))
+    """Comment this!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!"""
+    def __init__(self, _name):
+         self._name = _name
+
+    #getter method to return the instance variable _name for the particular Player (inherited in subclasses)
+    def name(self):
+         return self._name
+
+    """Comment this!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!Is self right param?"""
+    def play(self):
+         raise NotImplementedError("Not yet implemented")
