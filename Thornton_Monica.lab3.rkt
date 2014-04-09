441775e9020e331ca9c2327e3198755880396976
Monica Thornton <monicasuecooper@hotmail.com>
2014-04-08 21:12:50 -0600

    Scheme/Racket Program
    A program to build a number of functions, in order to learn and
    demonstrate functional programming.

---
+++ Thornton_Monica.lab3.rkt
@@ -0,0 +1,25 @@
+; tells DrRacket the language we are using (racket)
+#lang racket
+
+#|
+ ######################################## 
+ #  CSCI 305 - Programming Lab #3		
+ #										
+ #  Monica Thornton			
+ #  monicasuethornton@gmail.com			
+ #
+ # A program that contains a variety of different functions, 
+ # used to learn and demonstrate functional programming. 
+ #  
+ ######################################## 
+|#
+
+;(define (f lst)
+  ; (a) ;
+ ; (if (null? lst)
+      ; (b) ;
+  ;    ' ()
+      ; c; 
+   ;   (cons (+ 1 (car lst)) (f(cdr lst)))))
+
+
