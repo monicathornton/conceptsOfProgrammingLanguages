b6230bbf3e300cd82fa237d2666f68fd4bcf5610
Monica Thornton <monicasuecooper@hotmail.com>
2014-04-17 23:13:03 -0600

    Added member? function, started work on set?
    Added a function to check if an element is a member of a set. Updated
    comments on f function.

--- Thornton_Monica.lab3.rkt
+++ Thornton_Monica.lab3.rkt
@@ -1,5 +1,8 @@
-; tells DrRacket the language we are using (racket)
-#lang racket
+;Tells DrRacket the language we are using (Scheme);
+#lang scheme
+
+;Tells DrRacket that we want to enable tracing;
+(require racket/trace)
 
 #|
  ######################################## 
@@ -14,12 +17,60 @@
  ######################################## 
 |#
 
-;(define (f lst)
-  ; (a) ;
- ; (if (null? lst)
-      ; (b) ;
-  ;    ' ()
-      ; c; 
-   ;   (cons (+ 1 (car lst)) (f(cdr lst)))))
+
+#|Defines a function named f that given a list of numbers
+  (either integer or float) will increment that number by 1.
+  The function takes as its argument a list named lst.
+|#
+(define (f lst)
+  ;Allows us to trace the function f;
+  ;(trace f);
+  
+  ;Checks if lst is null (in other words, have we processed all values in lst yet?);
+  (if (null? lst)
+      ;If lst is empty, creates an empty list;
+      ' ()
+      #| Else, if lst is not empty - constructs a new list (via a recursive call to f), which contains the values in 
+         the old list incremented by 1.|#
+      (cons (+ 1 (car lst)) (f(cdr lst)))))
+
+#|Defines a function named member? that given a list
+  will search the list to determine if an element e 
+  (provided by the user) is a part of that list.
+  The function takes as its argument the target value e, 
+  and the list to be searched lst.
+|#
+(define (member? e lst)
+  ;Allows us to trace the function member?;
+  ;(trace member?);
+  
+  ;A series of checks to determine whether e is in the list ;
+  (cond 
+       #|Checks if the list is empty, both to avoid an error if the user provides an empty list 
+         initially, and to stop the recursive calls if the entire list is checked and the target element is 
+         not found. Returns #f in either case. 
+       |#
+       ((null? lst) #f)
+       ;Checks if the target is the first value in the list, if so returns #t to indicate element was in list.;
+       ((eqv? e (car lst)) #t)
+       #|If the list was not empty, and the target value was not the first item, recursively calls member? on the remainder 
+         of the list to determine if the target value occurs later in the list. With this default condition, no truth value
+         is returned.
+       |#
+       (else (member? e (cdr lst)))
+     )
+  )
 
 
+(define (set? lst)
+  ;Allows us to trace the function set?;
+  (trace set?)
+  
+  (cond 
+       ((null? lst) #t)
+  )
+   
+  
+  ((member? (car lst) (cdr lst)))
+     )
+  )
