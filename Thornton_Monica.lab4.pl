6898c6adc43a9cf446b8e9d6f6bd3861fe0ca5b1
Monica Thornton <monicasuecooper@hotmail.com>
2014-04-26 21:05:13 -0600

    Continued work on rules for family tree
    Still need to update comments, format.

--- Thornton_Monica.lab4.pl
+++ Thornton_Monica.lab4.pl
@@ -26,13 +26,41 @@ son(P,C):- child(P,C), male(C).
 %rule that defines a daughter as a child (C) who is female
 daughter(P,C):- child(P,C), female(C).
 
-% rule that defines a sibling of (X) as a child of the same parent
+%rule that defines a sibling of (X) as a child of the same parent
 sibling(X,Y):- father(F,X), father(F,Y), mother(M,X), mother(M,Y), not(same(X,Y)).
 
-brother(X,Y):- sibling(X,Y), male(X).
-sister(X,Y):- sibling(X,Y), female(X).
+%rule used to determine if two arguments are the same
 same(X,X).
 
+
+brother(B,Y):- sibling(B,Y), male(B).
+sister(S,Y):- sibling(S,Y), female(S).
+
+uncle(U,X):- sibling(U,P), parent(P,X), male(U).
+uncle(U,Y):- spouse(U,A), sibling(A,P), parent(P,Y), male(U).
+
+aunt(A,X):- sibling(A,P), parent(P,X), female(A).
+aunt(A,Y):- spouse(A,U), sibling(U,P), parent(P,Y), female(A).
+
+cousin(C,X):- parent(A,C), parent(B,X), sibling(A,B).
+
+grandparent(G,X):- parent(G,P), parent(P,X).
+
+grandfather(G,X):- grandparent(G,X), male(G).
+grandmother(G,X):- grandparent(G,X), female(G).
+
+grandchild(C,G):- grandparent(C,G).
+
+ancestor(A,D):- parent(A,D).
+ancestor(A,D):- parent(A,X), ancestor(X,D).
+
+descendant(D,A):- ancestor(A,D).
+
+older(O,Y):- born(O,X), born(Y,Z), (( X < Z) -> true ; false).
+
+younger(O,Y):- born(O,X), born(Y,Z), (( X < Z) -> false ; true).
+
+regentWhenBorn(R,B):- born(B,Y), reigned(R, S, E), S=<Y, Y=<E.
 
 
 
